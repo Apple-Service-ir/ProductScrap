2f93a497fd7290249b868a30559ba06a0878d44b
Wil1i <williams.ca001@gmail.com>
2023-04-01 01:49:21 +0430

    Rest API added| git ignore added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import utils.scrap as scrap
-
-print(scrap.get_products())
+from flask import Flask, jsonify
+ 
+app = Flask(__name__)
+ 
+@app.route('/divar', methods=['POST'])
+def index():
+    print("Request!")
+    data = scrap.get_products()
+    return data
+ 
+if __name__ == '__main__':
+    app.run()
